aa14816df3be371880ecd5137ab6c71c216dfb5d
Camilo Huertas <camiloandreshuertas26@gmail.com>
2023-01-31 20:04:41 -0500

    EstilosTituloAbout

--- src/components/AboutUs/About.jsx
+++ src/components/AboutUs/About.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import "./About.css"
 
 function About(props) {
   return (
     <div>
-        <h1>Hola about us</h1>
+        <h1 className="h1">Hola about us</h1>
     </div>
   );
 }
