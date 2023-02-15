5de02c02f8d6ae7ddbf6496fda00f3bb4fd36780
juanmoreno98 <juanmoreno17@gmail.com>
2023-02-14 20:24:42 -0500

    home

--- src/components/Home/indes.d.ts
+++ src/components/Home/indes.d.ts
@@ -1,6 +1,10 @@
 declare module '*.ts';
 declare module '*.tsx';
 declare module '*.jpeg';
+declare module '*.jpg';
 declare module '*.JPG';
 declare module '*.mp4';
+declare module '*.jsx';
+declare module '*.png';
+
 
