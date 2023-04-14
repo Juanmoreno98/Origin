6e301300e5b005a22b5d84eef43b5baadc1f0007
juanmoreno98 <juanmoreno17@gmail.com>
2023-04-13 21:06:23 -0500

    originI

--- src/App.js
+++ src/App.js
@@ -2,15 +2,13 @@ import React from "react";
 import { BrowserRouter as Router } from "react-router-dom";
 import { Routes, Route } from "react-router-dom";
 import About from "./components/AboutUs/About.jsx";
-import CardDetail from "./components/CardDetail/CardDetail.tsx";
 import ContactUs from "./components/ContactUs/ContactUs.tsx";
 import Footer from "./components/Footer/Footer.tsx";
 import Home from "./components/Home/Home.tsx";
 import Navbar from "./components/Navbar/Navbar.jsx";
-import Origin1 from "./components/Proyects/Origin1/Origin1.tsx";
+import Origin1 from "./components/Proyects/Origin1/Origin1.jsx";
 import Origin2 from "./components/Proyects/Origin2/Origin2";
 import Origin3 from "./components/Proyects/Origin3/Origin3.tsx";
-import Proyects from "./components/Proyects/Proyects.jsx";
 
 export default function App() {
   return (
@@ -22,7 +20,6 @@ export default function App() {
           <Route exact path="/about" element={<About />} />
           <Route path="/" element={<Footer />} />
           <Route exact path="/contactus" element={<ContactUs />} />
-          <Route exact path="/proyects" element={<Proyects />} />
           <Route exact path="projects/Origin1" element={<Origin1 />} />
           <Route exact path="projects/Origin2" element={<Origin2 />} />
           <Route exact path="projects/Origin3" element={<Origin3 />} />
